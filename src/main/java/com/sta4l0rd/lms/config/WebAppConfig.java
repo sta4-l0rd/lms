7f5e4fb05ec070c68c50ef87ef4457b2f3357459
astitwa-kumar <astitwa.kumar@u2opiamobile.com>
2025-06-19 01:44:31 +0530

    Interceptor Setup

---
+++ src/main/java/com/sta4l0rd/lms/config/WebAppConfig.java
@@ -0,0 +1,16 @@
+package com.sta4l0rd.lms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.sta4l0rd.lms.interceptor.LoggingInterceptor;
+
+@Configuration
+public class WebAppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor());
+    }
+}
