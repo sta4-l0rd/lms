b4a89326ec3284cae671dce60539cf6194f2d34e
astitwa-kumar <astitwa.kumar@u2opiamobile.com>
2025-06-19 18:01:19 +0530

    Interceptor package rename

--- src/main/java/com/sta4l0rd/lms/config/WebAppConfig.java
+++ src/main/java/com/sta4l0rd/lms/config/WebAppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.sta4l0rd.lms.interceptor.LoggingInterceptor;
+import com.sta4l0rd.lms.Interceptor.LoggingInterceptor;
 
 @Configuration
 public class WebAppConfig implements WebMvcConfigurer {
